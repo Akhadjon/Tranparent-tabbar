646706704d442afd5165f60c1d239d527f96cb12
Akhadjon Abdukhalilov <abduxalilovahadjon97@gmail.com>
2020-07-19 23:10:32 +0500

    Initial Commit

---
+++ TransparentUITabbar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransparentUITabbar
+//
+//  Created by Akhadjon Abdukhalilov on 7/19/20.
+//  Copyright © 2020 Akhadjon Abdukhalilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
